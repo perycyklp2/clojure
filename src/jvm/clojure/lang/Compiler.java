perycyklp2/clojure

commit 23c6d5f22b3b27346ac40ee9ff20e7b884414687

Allow loop/recur nested in catch and finally
Signed-off-by: Stuart Halloway <stu@thinkrelevance.com>

src/jvm/clojure/lang/Compiler.java
@@ -2133,7 +2133,7 @@ public static class TryExpr implements Expr{
                                             if(bodyExpr == null)
                                                 try {
                                                     Var.pushThreadBindings(RT.map(NO_RECUR, true));
-                                                    bodyExpr = (new BodyExpr.Parser()).parse(context, RT.seq(body));
+						    bodyExpr = (new BodyExpr.Parser()).parse(C.EXPRESSION, RT.seq(body));
                                                 } finally {
                                                     Var.popThreadBindings();
                                                 }
@@ -2159,7 +2159,7 @@ public static class TryExpr implements Expr{
 							                                (Symbol) (RT.second(f) instanceof Symbol ? RT.second(f)
 							                                                                         : null),
 							                                null,false);
-							Expr handler = (new BodyExpr.Parser()).parse(context, RT.next(RT.next(RT.next(f))));
+							Expr handler = (new BodyExpr.Parser()).parse(C.EXPRESSION, RT.next(RT.next(RT.next(f))));
 							catches = catches.cons(new CatchClause(c, lb, handler));
 							}
 						finally
@@ -2188,7 +2188,7 @@ public static class TryExpr implements Expr{
                             try 
                                 {
                                     Var.pushThreadBindings(RT.map(NO_RECUR, true));
-                                    bodyExpr = (new BodyExpr.Parser()).parse(context, RT.seq(body));
+				    bodyExpr = (new BodyExpr.Parser()).parse(C.EXPRESSION, RT.seq(body));
                                 } 
                             finally
                                 {
@@ -6144,8 +6144,6 @@ public static class RecurExpr implements Expr{
 			IPersistentVector loopLocals = (IPersistentVector) LOOP_LOCALS.deref();
 			if(context != C.RETURN || loopLocals == null)
 				throw new UnsupportedOperationException("Can only recur from tail position");
-			if(IN_CATCH_FINALLY.deref() != null)
-				throw new UnsupportedOperationException("Cannot recur from catch/finally");
                         if(NO_RECUR.deref() != null)
                             throw new UnsupportedOperationException("Cannot recur across try");
 			PersistentVector args = PersistentVector.EMPTY;

test/clojure/test_clojure/compilation.clj
@@ -61,6 +61,18 @@
     (is (thrown? Compiler$CompilerException
                  (eval '(loop [x 5]
                           (try (recur 1)))))))
+  (testing "don't recur to loop from inside of catch inside of try"
+    (is (thrown? Compiler$CompilerException
+                 (eval '(loop [x 5]
+                          (try
+                            (catch Exception e
+                              (recur 1))))))))
+  (testing "don't recur to loop from inside of finally inside of try"
+    (is (thrown? Compiler$CompilerException
+                 (eval '(loop [x 5]
+                          (try
+                            (finally
+                              (recur 1))))))))
   (testing "don't get confused about what the recur is targeting"
     (is (thrown? Compiler$CompilerException
                  (eval '(loop [x 5]
@@ -71,6 +83,20 @@
   (testing "allow loop/recur inside try"
     (is (= 0 (eval '(try (loop [x 3]
                            (if (zero? x) x (recur (dec x)))))))))
+  (testing "allow loop/recur fully inside catch"
+    (is (= 3 (eval '(try
+                      (throw (Exception.))
+                      (catch Exception e
+                        (loop [x 0]
+                          (if (< x 3) (recur (inc x)) x))))))))
+  (testing "allow loop/recur fully inside finally"
+    (is (= "012" (eval '(with-out-str
+                          (try
+                            :return-val-discarded-because-of-with-out-str
+                            (finally (loop [x 0]
+                                       (when (< x 3)
+                                         (print x)
+                                         (recur (inc x)))))))))))
   (testing "allow fn/recur inside try"
     (is (= 0 (eval '(try
                       ((fn [x]

commit 0564521246f2071a78e77b869af0c658b74fc7d7

fix for leak caused by 23c6d5f22b3b2734

.idea/libraries/Maven__org_clojure_data_generators_0_1_2.xml
@@ -1,0 +1,13 @@
+<component name="libraryTable">
+  <library name="Maven: org.clojure:data.generators:0.1.2">
+    <CLASSES>
+      <root url="jar://$MAVEN_REPOSITORY$/org/clojure/data.generators/0.1.2/data.generators-0.1.2.jar!/" />
+    </CLASSES>
+    <JAVADOC>
+      <root url="jar://$MAVEN_REPOSITORY$/org/clojure/data.generators/0.1.2/data.generators-0.1.2-javadoc.jar!/" />
+    </JAVADOC>
+    <SOURCES>
+      <root url="jar://$MAVEN_REPOSITORY$/org/clojure/data.generators/0.1.2/data.generators-0.1.2-sources.jar!/" />
+    </SOURCES>
+  </library>
+</component>

.idea/libraries/Maven__org_clojure_test_generative_0_4_0.xml
@@ -1,0 +1,13 @@
+<component name="libraryTable">
+  <library name="Maven: org.clojure:test.generative:0.4.0">
+    <CLASSES>
+      <root url="jar://$MAVEN_REPOSITORY$/org/clojure/test.generative/0.4.0/test.generative-0.4.0.jar!/" />
+    </CLASSES>
+    <JAVADOC>
+      <root url="jar://$MAVEN_REPOSITORY$/org/clojure/test.generative/0.4.0/test.generative-0.4.0-javadoc.jar!/" />
+    </JAVADOC>
+    <SOURCES>
+      <root url="jar://$MAVEN_REPOSITORY$/org/clojure/test.generative/0.4.0/test.generative-0.4.0-sources.jar!/" />
+    </SOURCES>
+  </library>
+</component>

clojure.iml
@@ -12,15 +12,15 @@
       <sourceFolder url="file://$MODULE_DIR$/src/resources" isTestSource="false" />
       <sourceFolder url="file://$MODULE_DIR$/src/clj" isTestSource="false" />
       <sourceFolder url="file://$MODULE_DIR$/test/java" isTestSource="true" />
-      <sourceFolder url="file://$MODULE_DIR$/src/jvm" isTestSource="false" />
       <excludeFolder url="file://$MODULE_DIR$/target" />
     </content>
     <orderEntry type="inheritedJdk" />
     <orderEntry type="sourceFolder" forTests="false" />
     <orderEntry type="library" scope="PROVIDED" name="Maven: org.codehaus.jsr166-mirror:jsr166y:1.7.0" level="project" />
-    <orderEntry type="library" scope="TEST" name="Maven: org.clojure:test.generative:0.2.0" level="project" />
+    <orderEntry type="library" scope="TEST" name="Maven: org.clojure:test.generative:0.4.0" level="project" />
     <orderEntry type="library" scope="TEST" name="Maven: org.clojure:tools.namespace:0.1.1" level="project" />
     <orderEntry type="library" scope="TEST" name="Maven: org.clojure:java.classpath:0.1.1" level="project" />
+    <orderEntry type="library" scope="TEST" name="Maven: org.clojure:data.generators:0.1.2" level="project" />
   </component>
 </module>
 

src/jvm/clojure/lang/Compiler.java
@@ -2153,7 +2153,7 @@ public static class TryExpr implements Expr{
                                             if(bodyExpr == null)
                                                 try {
                                                     Var.pushThreadBindings(RT.map(NO_RECUR, true));
-						    bodyExpr = (new BodyExpr.Parser()).parse(C.EXPRESSION, RT.seq(body));
+						                            bodyExpr = (new BodyExpr.Parser()).parse(context, RT.seq(body));
                                                 } finally {
                                                     Var.popThreadBindings();
                                                 }